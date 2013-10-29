385f05b36e2572e4ad3d15e40c7e2a6fb6d9d0fc
Jenny Yee <jennyyee@Jennycat.local>
2013-10-28 21:45:03 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Shirelist::Application.config.secret_token = '033522d22eb8447c5d38e3b3d386fd18ef6db309baf3871a714f5faf4bdbcf7c46b9ff1b55c73f1ec594004ec0435c62927cc150d2b035ba49a731eb99d7145f'
