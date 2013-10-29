b62acdd7cfdff4782b3855f1a6173581cbd13cd3
Jenny Yee <jennyyee@Jennycat.local>
2013-10-28 21:55:37 -0700

    Added database

---
+++ db/migrate/20131029044651_create_listings.rb
@@ -0,0 +1,17 @@
+class CreateListings < ActiveRecord::Migration
+  def up
+    create_table :listings do |t|
+      t.string :title
+      t.string :description
+      t.string :price
+      t.string :user_id
+      t.string :item_key
+
+      t.timestamps
+    end
+  end
+
+  def down
+    drop_table :listings
+  end
+end
